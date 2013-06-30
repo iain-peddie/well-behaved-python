09ad58be7788f51a1e530d2f5299aa2d5214a0c4
Iain Peddie <inr314159@hotmail.com>
2013-06-30 16:58:16 +0100

    Final changes to bootstrap file

--- tests/bootstrap.py
+++ tests/bootstrap.py
@@ -15,14 +15,7 @@ def setup():
     cwd = os.curdir
     srcPath = os.path.join(cwd, '../src/')
     srcPath = os.path.abspath(srcPath)
-    sys.path.append(srcPath)
-    
-
-def test_whether_test_is_run():
-    tester = WasRun()
-    test = it("is a test", tester.targetMethod)
-    test.run()
-    assert tester.WasRun
+    sys.path.append(srcPath)    
 
 if __name__ == "__main__":
     setup()
