06080636271ec4aebfc92da484a3365b6167ef67
Iain Peddie <inr314159@hotmail.com>
2013-06-30 18:07:51 +0100

    Refactored tests to examine log string

--- tests/TestCaseTests.py
+++ tests/TestCaseTests.py
@@ -9,19 +9,26 @@ class TestCaseTests(TestCase):
 
     def __init__(self, testFunctionName):
         TestCase.__init__(self, testFunctionName)
+        self.log = ""
 
     def before(self):
         self.wasBeforeCalled = True
+        self.log += "before "
 
     def targetMethod(self):
         """Target method when running unit tests."""
         self.wasTargetMethodCalled = True
+        self.log += "targetMethod "
 
     def test_run_template(self):
-        self.targetMethod()
+        # we can't use self shunt here, because we need to
+        # test for the after case. If we were self-shunting,
+        # then after would be called after this test, which
+        # would mean we couldn't test for it...
+        test = TestCaseTests("targetMethod")
+        test.run()
         
-        assert self.wasBeforeCalled
-        assert self.wasTargetMethodCalled        
+        assert test.log == "before targetMethod "
         
 if __name__ == "__main__":
     TestCaseTests("test_run_template").run()
