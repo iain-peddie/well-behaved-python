060ded1cf04e3dcef570fef64f5dc7861568b86f
Iain Peddie <inr314159@hotmail.com>
2013-07-04 08:14:08 +0100

    Created an ExpectsNot class, with opposite semantics for pass and fail

---
+++ src/WellBehavedPython/ExpectNot.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Copyright 2013 Iain Peddie inr314159@hotmail.com
+# 
+#    This file is part of WellBehavedPython
+#
+#    WellBehavedPython is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    WellBehavedPython is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with WellBehavedPython. If not, see <http://www.gnu.org/licenses/>.
+
+class ExpectNot:
+    """Class used to indicate the opposite of expected outcomes."""
+
+    def __init__(self, actual):
+        self.actual = actual
+
+    def fail(self, Message = ""):
+        """Indicate a failure.
+        
+        not fail should have the semantics of success."""
+        pass
+
+    def success(self, Message = ""):
+        """Opposite semantincs to fail. Would be pass if pass was not a 
+        reserved keyword.
+
+        Not success is actually a failure contition. This should have the
+        same semantics as Expect.fail()"""
+        raise AssertionError(Message)
+
+    def toEqual(self, expected):
+        pass
---
+++ tests/ExpectNotTests.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Copyright 2013 Iain Peddie inr314159@hotmail.com
+# 
+#    This file is part of WellBehavedPython
+#
+#    WellBehavedPython is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    WellBehavedPython is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with WellBehavedPython. If not, see <http://www.gnu.org/licenses/>.
+
+from WellBehavedPython.TestCase import *
+from WellBehavedPython.TestSuite import *
+from WellBehavedPython.ExpectNot import *
+from WellBehavedPython.Expect import *
+
+class ExpectNotTests(TestCase):
+    
+    def __init__(self, testFunctionName):
+        TestCase.__init__(self, testFunctionName)
+
+    @staticmethod
+    def suite():
+        testMethods = [
+            "test_fail_doesnt_raise_anything",
+            "test_success_raises_AssertionError",            
+#            "test_equals_doesnt_raise_if_numeric_items_are_equal",
+#            "test_equals_raises_with_right_message_if_numeric_items_not_equal",
+#            "test_equals_doesnt_raise_if_string_items_are_equal",
+#            "test_equals_raises_with_right_message_if_string_items_not_equal"
+            ]
+        
+        suite = TestSuite()
+    
+        for testMethod in testMethods:
+            suite.add(ExpectNotTests(testMethod))
+        return suite
+
+    def test_fail_doesnt_raise_anything(self):
+        ExpectNot(True).fail()
+        # pass condition should be that we get to this point
+
+    def test_success_raises_AssertionError(self):
+        flag = True
+        try:
+            ExpectNot(True).success("Message")
+            flag = False
+        except AssertionError as ex:
+            Expect(ex.args[0]).toEqual("Message")
+        assert flag, "Expected AssertionError to have been thrown, but nothing was"
+
+if __name__ == "__main__":
+    suite = ExpectNotTests.suite()
+    results = TestResults()
+    suite.run(results)
+    
+    print(results.summary())
