0394c8289e0ba2d4f9168aa4899f9273e15a795b
Iain Peddie <inr314159@hotmail.com>
2013-07-10 07:50:22 +0100

    Instance comparison works in Expects with direct comparisons

--- tests/ExpectTests.py
+++ tests/ExpectTests.py
@@ -251,10 +251,34 @@ class ExpectTests(TestCase):
             message = ex.args[0]
         Expect(message).toEqual("user message: Expected [601, 603, 605] to contain 602")
 
+    def test_1_instanceof_int_passes(self):
+        Expect(1).toBeAnInstanceOf(int)
+
+    def test_1_instanceof_float_fails(self):
+        message = ""
+        try:
+            Expect(1).toBeAnInstanceOf(float)
+        except AssertionError as ex:
+            message = ex.args[0]
+        Expect(message).toEqual("Expected 1 to be an instance of <class 'float'> but was an instance of <class 'int'>")
+
+    def test_instance_of_userclass_passes(self):
+        Expect(TestResults()).toBeAnInstanceOf(TestResults)
+
+    def test_instance_of_wrong_userclass_fails(self):
+        message = ""
+        try:
+            Expect(TestResults()).toBeAnInstanceOf(TestSuite)
+        except AssertionError as ex:
+            message = ex.args[0]
+        Expect(message).toEqual("Expected <WellBehavedPython.TestResults.TestResults object> to be an instance of <class 'WellBehavedPython.TestSuite.TestSuite'> but was an instance of <class 'WellBehavedPython.TestResults.TestResults'>")
+
+
 if __name__ == "__main__":
     suite = ExpectTests.suite()
     results = TestResults()
     suite.run(results)
     
     print(results.summary())
+
 
