cfd5e610fd19409830f6317ee893b29dcaf4b827
Iain Peddie <inr314159@hotmail.com>
2013-08-19 20:09:42 +0100

    Test results stores that tests were ignored. Simplified tests of test results

--- tests/AllTests.py
+++ tests/AllTests.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+
 from TestSuiteTests import *
 from TestCaseTests import *
 from TestResultsTests import *
@@ -11,15 +13,22 @@ from WellBehavedPython.TestSuite import TestSuite
 from WellBehavedPython.ConsoleTestRunner import ConsoleTestRunner
 
 if __name__ == "__main__":
-    results = TestResults()
-    runner = ConsoleTestRunner()
-    suite = TestSuite()
-    suite.add(TestResultsTests.suite())
-    suite.add(TestCaseTests.suite())
-    suite.add(TestSuiteTests.suite())
-    suite.add(ExpectTests.suite())
-    suite.add(ExpectNotTests.suite())
-    suite.add(ConsoleTestRunnerTests.suite())
-    results = runner.run(suite)
+    try:
+        results = TestResults()
+        runner = ConsoleTestRunner(bufferOutput = True)
+        suite = TestSuite()
+        suite.add(TestResultsTests.suite())
+        suite.add(TestCaseTests.suite())
+        suite.add(TestSuiteTests.suite())
+        suite.add(ExpectTests.suite())
+        suite.add(ExpectNotTests.suite())
+        suite.add(ConsoleTestRunnerTests.suite())
+        results = runner.run(suite)
 
-    exit(results.failCount > 0)
+        exit(results.failCount > 0)
+    except Exception as ex:
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+        traceback.print_exc(file = sty.stdout)
+        
+    
