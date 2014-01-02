c4cae2da1ab605b3413217ef7f72461dfa703bab
Iain Peddie <inr314159@hotmail.com>
2014-01-02 16:01:57 +0000

    Created a setup.py script for distibution

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from distutils.dir_util import *
+
+try:
+    copy_tree('src/WellBehavedPython', 'WellBehavedPython')
+
+    setup(
+        name = 'WellBehavedPython', 
+        packages = ['WellBehavedPython'],
+        version = '0.5.0',
+        description = 'Testing package with fluent API', 
+        author = 'Iain Peddie', 
+        author_email = 'inr314159@hotmail.com', 
+        url = 'https://github.com/iain-peddie/well-behaved-python',
+        classifiers = [
+            'Language :: Python', 
+            'Language :: Python :: 3.3',
+            'Licence :: OSI Approved :: GNU General Public Licence v3 or later (GPLv3+)',         
+            'Development Status :: 4 - Beta', 
+            'Environment :: Console ',
+            'Intended Audience :: Developers', 
+            'Operating System :: OS Independent', 
+            'Topic :: Software Development :: Testing']
+        )
+
+finally:
+    remove_tree('WellBehavedPython')
